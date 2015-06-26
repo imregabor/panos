d95f3438febc7b4ff6cc0a4be87e1d4b1c57c8c6
Gabor Imre <imregabor@gmail.com>
2015-06-26 17:55:29 +0200

    FHD with higher/lower bitrate added

--- make-video-tmbdir.sh
+++ make-video-tmbdir.sh
@@ -3,8 +3,12 @@
 set -e
 
 PWD=$(pwd)
+
+TDIRFHL="../"$(basename "${PWD}")"-fhd-avi-h264-v1000-a128/"
+TDIRFH="../"$(basename "${PWD}")"-fhd-avi-h264-v1500-a192/"
 TDIRHI="../"$(basename "${PWD}")"-avi-h264-v1500-a192/"
 TDIRLO="../"$(basename "${PWD}")"-flv-v300-a64/"
+
 
 
 find -wholename "*.MTS" -or -wholename "*.MOV" | while read infile
@@ -44,17 +48,23 @@ do
     
     eval "$probe"
 
+    dofhd=""
     
     if [ "${streams_stream_0_width}" == "1920" ] && [ "${streams_stream_0_height}" == "1080" ] 
     then
         scalehi="-s 1280x720"
         scalelo="-s 640x360"
         aspect="-aspect 16:9"
+        dofhd="true"
+        scalefhd=""
+
     elif [ "${streams_stream_0_width}" == "1280" ] && [ "${streams_stream_0_height}" == "720" ]         
     then
         scalehi=""
         scalelo="-s 640x360"
         aspect="-aspect 16:9"
+        scalefhd=""
+        dofhd=""
     else
         echo "UNKNOWN RESOULUTION"
         exit
@@ -73,58 +83,132 @@ do
     
     TDH="${TDIRHI}/${dir}"
     TDL="${TDIRLO}/${dir}"
+    TDF="${TDIRFH}/${dir}"
+    TDFHL="${TDIRFHL}/${dir}"
     TFL="${TDL}/${fil}.flv"
     TFH="${TDH}/${fil}.avi"
+    TFF="${TDF}/${fil}.avi"
+    TFFHL="${TDFHL}/${fil}.avi"
     
+    OPTSFHL="-vcodec libx264 ${rate} ${scalefhd} ${aspect} -b 1000k -acodec libmp3lame -ac 2 -ar 44100 -ab 128k"
+    OPTSFH="-vcodec libx264 ${rate} ${scalefhd} ${aspect} -b 1500k -acodec libmp3lame -ac 2 -ar 44100 -ab 192k"
     OPTSHI="-vcodec libx264 ${rate} ${scalehi} ${aspect} -b 1500k -acodec libmp3lame -ac 2 -ar 44100 -ab 192k"
     OPTSLO="-vcodec flv -f flv ${rate} ${scalelo} ${aspect} -b 300k -g 160 -cmp 2 -subcmp 2 -mbd 2 -trellis 2 -acodec libmp3lame -ac 2 -ar 22050 -ab 64k"
 
     echo "Video specific settings:"
+    echo
+    echo "    dofhd:                 \"$dofhd\""
     echo 
-    echo "    Scaling in high:      \"$scalehi\""
-    echo "    Scaling in low:       \"$scalelo\""
-    echo "    Aspect:               \"$aspect\""
-    echo "    Framerate:            \"$rate\""
+    echo "    Scaling in fhd:        \"$scalefhd\""
+    echo "    Scaling in high:       \"$scalehi\""
+    echo "    Scaling in low:        \"$scalelo\""
+    echo "    Aspect:                \"$aspect\""
+    echo "    Framerate:             \"$rate\""
     echo
-    echo "    Target directory hi:  \"${TDH}\""
-    echo "    Target directory lo:  \"${TDL}\""
-    echo "    Target file hi:       \"${TFH}\""
-    echo "    Target file lo:       \"${TFL}\""
+    echo "    Target directory fhd:  \"${TDF}\""
+    echo "    Target directory hi:   \"${TDH}\""
+    echo "    Target directory lo:   \"${TDL}\""
+    echo "    Target file fhd:       \"${TFF}\""
+    echo "    Target file hi:        \"${TFH}\""
+    echo "    Target file lo:        \"${TFL}\""
     echo
-    echo "    Full options line hi: \"${OPTSHI}\""
-    echo "    Full options line lo: \"${OPTSLO}\""
+    echo "    Full options line fhd: \"${OPTSFH}\""
+    echo "    Full options line hi:  \"${OPTSHI}\""
+    echo "    Full options line lo:  \"${OPTSLO}\""
     
     
-    mkdir -p "${TDH}"
-    mkdir -p "${TDL}"
-    
-    echo
-    echo
-    echo
-    echo "+------------------------------------------------------------------------------------------------------------"
-    echo "| Launch ffmpeg on lo"
-    echo "+------------------------------------------------------------------------------------------------------------"
-    echo
-    echo
-    echo
+
+
+
     if [ ! -e "$TFL" ]
     then
+        echo
+        echo
+        echo
+        echo "+------------------------------------------------------------------------------------------------------------"
+        echo "| Launch ffmpeg on lo"
+        echo "|"
+        echo "|       input file:  \"$infile\""
+        echo "|       output file: \"$TFL\""
+        echo "+------------------------------------------------------------------------------------------------------------"
+        echo
+        echo
+        echo
+        mkdir -p "${TDL}"
         # see http://mywiki.wooledge.org/BashFAQ/089
         ffmpeg -i "$infile" $OPTSLO "$TFL" </dev/null
+    else
+        echo "$TFL exists."
     fi
     
-    echo
-    echo
-    echo
-    echo "+------------------------------------------------------------------------------------------------------------"
-    echo "| Launch ffmpeg on hi"
-    echo "+------------------------------------------------------------------------------------------------------------"
-    echo
-    echo
-    echo
     
     if [ ! -e "$TFH" ]
     then
+        echo
+        echo
+        echo
+        echo "+------------------------------------------------------------------------------------------------------------"
+        echo "| Launch ffmpeg on hi"
+        echo "|"
+        echo "|       input file:  \"$infile\""
+        echo "|       output file: \"$TFH\""
+        echo "+------------------------------------------------------------------------------------------------------------"
+        echo
+        echo
+        echo
+        mkdir -p "${TDH}"
         ffmpeg -i "$infile" $OPTSHI "$TFH" </dev/null
+    else
+        echo "$TFH exists"
     fi
+
+
+    if [ ! -z "$dofhd" ] 
+    then
+        # FHD run
+        if [ ! -e "$TFF" ]
+        then
+            echo
+            echo
+            echo
+            echo "+------------------------------------------------------------------------------------------------------------"
+            echo "| Launch ffmpeg on fhd"
+            echo "|"
+            echo "|       input file:  \"$infile\""
+            echo "|       output file: \"$TFF\""
+            echo "+------------------------------------------------------------------------------------------------------------"
+            echo
+            echo
+            echo
+            mkdir -p "${TDF}"
+            # see http://mywiki.wooledge.org/BashFAQ/089
+            ffmpeg -i "$infile" $OPTSFH "$TFF" </dev/null
+        else
+            echo "$TFF exists."
+        fi
+
+
+        if [ ! -e "$TFFHL" ]
+        then
+            echo
+            echo
+            echo
+            echo "+------------------------------------------------------------------------------------------------------------"
+            echo "| Launch ffmpeg on fhd - lower bitrate"
+            echo "|"
+            echo "|       input file:  \"$infile\""
+            echo "|       output file: \"$TFFHL\""
+            echo "+------------------------------------------------------------------------------------------------------------"
+            echo
+            echo
+            echo
+            mkdir -p "${TDFHL}"
+            # see http://mywiki.wooledge.org/BashFAQ/089
+            ffmpeg -i "$infile" $OPTSFHL "$TFFHL" </dev/null
+        else
+            echo "$TFFHL exists."
+        fi
+
+    fi
+
 done    
