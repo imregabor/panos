f9cd2f95f195921cdf895235f351a9d6450b40fb
Gabor Imre <imregabor@gmail.com>
2015-08-02 00:20:42 +0200

    --center arg in a variable

--- pano-smaller.sh
+++ pano-smaller.sh
@@ -19,9 +19,11 @@ LOG=pano.log
 # Initial settings
 # PMFOV=""
 # PMCRP=""
+# PMCTR=""
 
 PMFOV="--fov=AUTO"
 PMCRP="--crop=AUTO"
+PMCTR="--center"
 
 function sect {
     echo | tee -a "${LOG}" ; echo | tee -a "${LOG}" ; echo "+------------------------------------------------------------------------------------------" | tee -a "${LOG}" ; echo "|" | tee -a "${LOG}"
@@ -69,16 +71,16 @@ PI="${PO}" ; PO=sources/pano-02-cp.pto          ; sect "cpfind"                 
 PI="${PO}" ; PO=sources/pano-03-celeste.pto     ; sect "celeste"                   "PI: ${PI}" "PO: ${PO}" ; celeste_standalone -i "${PI}" -o "${PO}"                                                    2>&1 | tee -a "${LOG}"
 PI="${PO}" ; PO=sources/pano-04-cpclean.pto     ; sect "cpclean"                   "PI: ${PI}" "PO: ${PO}" ; cpclean -o "${PO}" "${PI}"                                                                  2>&1 | tee -a "${LOG}"
 PI="${PO}" ; PO=sources/pano-05-pwopted.pto     ; sect "do pairwise optimization"  "PI: ${PI}" "PO: ${PO}" ; autooptimiser -p -o "${PO}" "${PI}"                                                         2>&1 | tee -a "${LOG}"
-PI="${PO}" ; PO=sources/pano-06-pwopted-m.pto   ; sect "modify: str; cnt; fov"     "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten --center $PMFOV $PMCRP "${PI}"                             2>&1 | tee -a "${LOG}"
+PI="${PO}" ; PO=sources/pano-06-pwopted-m.pto   ; sect "modify: str; cnt; fov"     "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten $PMCTR $PMFOV $PMCRP "${PI}"                             2>&1 | tee -a "${LOG}"
 # blprev "${PO}"
 PI="${PO}" ; PO=sources/pano-07-geomopt.pto     ; sect "set geomopt"               "PI: ${PI}" "PO: ${PO}" ; pto_var -o "${PO}" --opt y,p,r,v,a,b,c "${PI}"                                              2>&1 | tee -a "${LOG}"
 PI="${PO}" ; PO=sources/pano-08-geomopted.pto   ; sect "do geomopt"                "PI: ${PI}" "PO: ${PO}" ; autooptimiser -n -o "${PO}" "${PI}"                                                         2>&1 | tee -a "${LOG}"
-PI="${PO}" ; PO=sources/pano-09-geomopted-m.pto ; sect "modify"                    "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten --center $PMFOV $PMCRP "${PI}"                             2>&1 | tee -a "${LOG}"
+PI="${PO}" ; PO=sources/pano-09-geomopted-m.pto ; sect "modify"                    "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten $PMCTR $PMFOV $PMCRP "${PI}"                             2>&1 | tee -a "${LOG}"
 # blprev "${PO}"
 PI="${PO}" ; PO=sources/pano-10-vignopt.pto     ; sect "set photoopt (vign)"       "PI: ${PI}" "PO: ${PO}" ; pto_var -o "${PO}" --modify-opt --opt Ra,Rb,Rc,Rd,Re,Vb,Vc,Vd,Vx,Vy "${PI}"                 2>&1 | tee -a "${LOG}"
 PI="${PO}" ; PO=sources/pano-11-vignopted.pto   ; sect "do photometric opt"        "PI: ${PI}" "PO: ${PO}" ; vig_optimize -o "${PO}" -v -p 5000 "${PI}"                                                  2>&1 | tee -a "${LOG}"
 # blprev "${PO}"
-PI="${PO}" ; PO=sources/pano-12-modified.pto    ; sect "final modify"              "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten --center --canvas=AUTO $PMFOV $PMCRP "${PI}"   2>&1 | tee -a "${LOG}"
+PI="${PO}" ; PO=sources/pano-12-modified.pto    ; sect "final modify"              "PI: ${PI}" "PO: ${PO}" ; pano_modify -o "${PO}" --straighten $PMCTR --canvas=AUTO $PMFOV $PMCRP "${PI}"   2>&1 | tee -a "${LOG}"
 
 # Final blend
 blprev "${PO}"
