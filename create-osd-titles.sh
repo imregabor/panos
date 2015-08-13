ef8ee51d3ca479ae83e548a9caadd1f51a730530
Gabor Imre <imregabor@gmail.com>
2015-08-13 14:13:56 +0200

    Rework dzi generation

--- create-osd-titles.sh
+++ create-osd-titles.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 #
-# Create openseadragon titles
+# Create openseadragon-compatible DZI image pyramid titles
 #
-#
+# See:
+#    https://openseadragon.github.io/
+#    https://openseadragon.github.io/examples/tilesource-dzi/
+#    https://msdn.microsoft.com/en-us/library/cc645077%28v=vs.95%29.aspx
+#    https://msdn.microsoft.com/en-us/library/cc645022%28v=vs.95%29.aspx
+
 set -e
 set -o pipefail
 set -u
 
+#
+# Check tools to bo used
+#
 for i in convert identify
 do
     echo "Checking command $i"
     command -v $i >/dev/null 2>&1 || { echo >&2 "$i not found"; exit 1; }
-done    
+done
 
-if [ $# -ne 1 ] ; then echo "Specify image to process." ; exit -1 ; fi
-if [ ! -e "$1" ] ; then echo "Input image \"$1\" not found." ; exit -1 ; fi
+usage() {
+    echo "usage:"
+    echo
+    echo "    $0 -i <INPUTFILE> -o <OUTPUTPREFIX> [-t <TITLESIZE>]"
+    echo
+}
 
-B=$(basename "$1" .tif)
-B=$(basename "$B" .TIF)
-B=$(basename "$B" .jpg)
-B=$(basename "$B" .JPG)
-B=$(basename "$B" .jpeg)
-B=$(basename "$B" .JPEG)
-B=$(basename "$B" .png)
-B=$(basename "$B" .PNG)
-B=$(echo "$B" | tr " " "_")
-echo "Image basename: $B"
+#
+# Default values; pars CLI argumentgs
+#
 
-
+IN=""
+OUT=""
 T=2048
 
-echo "Tile size:      $T"
+while getopts "i:o:t:" OPTION ; do
+    case $OPTION in
+    i) IN="${OPTARG}" ;;
+    o) OUT="${OPTARG}" ;;
+    t) T="${OPTARG}" ;;
+    *) usage ; exit -1 ;;
+esac ; done
 
+if [ -z "${IN}" ] ; then echo "Input not specified; use -i <INFILE>" ; exit -1 ; fi
+if [ -z "${OUT}" ] ; then echo "Output prefix is not specified; use -o <OUTPUTPREFIX>" ; exit -1 ; fi
+if [ ! -e "${IN}" ] ; then echo "Input image \"${IN}\" not found." ; exit -1 ; fi
+
+DZI="${OUT}.dzi"
+
+
+echo "------------------------------------------------------------------------------------"
+echo "Create image pyramid titles"
+echo
+echo "   Input image:    \"$IN\""
+echo "   Output prefix:  \"$OUT\""
+echo "   Tile size:      $T"
+echo "   DZI file:       \"$DZI\""
+echo "------------------------------------------------------------------------------------"
+echo
+echo
 
 
 processSingleLevel() {
@@ -41,7 +70,9 @@ processSingleLevel() {
     echo "--------------------------------------------------"
     echo
     echo
-    
+
+    # Retrieve and check image dimensions
+
     echo "    Check image dimensions:"
     W=$(identify -format "%w" "$1")
     H=$(identify -format "%h" "$1")
@@ -51,14 +82,16 @@ processSingleLevel() {
     if [ $W -gt $H ] ; then G=$W ; else G=$H ; fi
     echo "        greater: $G"
 
+    # Determine image level
+
     L=0 ; I=1; while [ $G -gt $I ] ; do L=$((L + 1)) ; I=$((I * 2)) ; done
     echo "        image level: $L"
 
 
+    OUTDIR="${OUT}_files/${L}"
+    mkdir -p "${OUTDIR}"
 
-    mkdir -p "${B}_files/${L}"
-
-    echo "    Create tiles"
+    echo "    Create tiles into output directory \"${OUTDIR}\""
     
     Y=0
     while true ; do
@@ -76,9 +109,9 @@ processSingleLevel() {
             
             
             
-            OF="${B}_files/${L}/${X}_${Y}.jpg"
+            OF="${OUTDIR}/${X}_${Y}.jpg"
             
-            echo "        X: $X X1: $X1 Y: $Y Y1: $Y1 CW: $CW CH: $CH OF: $OF"
+            echo "        Tile index: X: $X Y: $Y Tile upper left corner: X1: $X1  Y1: $Y1 Tile size: CW: $CW CH: $CH Output file: $OF"
             
             if [ ! -e "${OF}" ]
             then
@@ -90,22 +123,49 @@ processSingleLevel() {
         Y=$(($Y + 1))
     done
 
+    echo "        Tiles created."
 } # processSingleLevel
 
-processSingleLevel "$1"
+processSingleLevel "$IN"
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
-<Image xmlns=\"http://schemas.microsoft.com/deepzoom/2008\" Format=\"jpg\" Overlap=\"0\" TileSize=\"${T}\">\
-    <Size Width=\"${W}\" Height=\"${H}\"/>\
-</Image>" > "${B}.dzi"
+# Write DZI file containing image dimensions
+echo -e "<?xml version='1.0' encoding='UTF-8'?>\n\
+<!-- This is an auto generated DZI file -->\n\
+<Image xmlns='http://schemas.microsoft.com/deepzoom/2008' Format='jpg' Overlap='0' TileSize='${T}'>\n\
+    <Size Width='${W}' Height='${H}'/>\n\
+</Image>" > "${DZI}"
+
+# Write index HTML
+echo -e "<!DOCTYPE html>\n\
+<html>\n\
+<head>\n\
+    <title>Zoomable image</title>\n\
+    <script src='js/openseadragon-bin-2.0.0/openseadragon.min.js'></script>\n\
+</head>\n\
+<body>\n\
+    <div id='img' style='position: fixed; top: 0; left: 0; bottom: 0; right: 0'></div>\n\
+    <script type='text/javascript'>\n\
+        OpenSeadragon({\n\
+            id:              'img',\n\
+            prefixUrl:       'js/openseadragon-bin-2.0.0/images/',\n\
+            tileSources:     'tiles.dzi',\n\
+        });\n\
+    </script>\n\
+</body>" > "${OUT}.html"
+
+# Create JS dir
+if [ ! -d "js/" ]
+then
+    mkdir js/
+    wget https://github.com/openseadragon/openseadragon/releases/download/v2.0.0/openseadragon-bin-2.0.0.tar.gz -O - | tar -xz -C js/
+fi
 
 
-
-IN="$1"
+# Process further levels
 while true ; do
     
-    
-    if [ $L -lt 5 ]
+    # Do not descend into too small levels
+    if [ $L -lt 6 ]
     then
         echo "Done."
         break
@@ -116,18 +176,29 @@ while true ; do
     if [ $NW -lt 1 ] ; then NW=1 ; fi
     if [ $NH -lt 1 ] ; then NH=1 ; fi
     
-    OUT="tmp-L-"$(($L - 1))".jpg"
+    mkdir -p "${OUT}_tmbs"
+    TMB="${OUT}_tmbs/L-"$(($L - 1))".jpg"
     SIZ=${NW}x${NH}
     
+    echo 
+    echo "--------------------------------------------------"
+    echo "Create next level tiling"
+    echo
+    echo "    Current level: $L"
+    echo "    Current size:  $W x $H"
+    echo "    Next size:     $NW x $NH"
+    echo
+    echo "    Resze arg:     $SIZ"
+    echo "    Thumbnail:     $TMB"
+    echo "--------------------------------------------------"
     echo
     echo
-    echo "Next level: ${OUT} with ${SIZ}"
-    echo
-    echo
-    
-    
-    convert -resize ${SIZ} "${IN}" "${OUT}"
-    IN="${OUT}"
-    
+
+    if [ ! -e "${TMB}" ]
+    then
+        convert -resize ${SIZ} "${IN}" "${TMB}"
+    fi
+    IN="${TMB}"
+
     processSingleLevel "${IN}"
 done
