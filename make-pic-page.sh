581685dedb855b2e897ab351c00c8a3670f18d68
Gabor Imre <imregabor@gmail.com>
2018-03-27 20:16:02 +0200

    Migrated xsl based image index / preview creation

---
+++ make-pic-page.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+set -e
+# set -o pipefail
+set -u
+
+
+echo "================================================================================================================="
+echo
+echo "Create pano/pic overview page"
+echo
+echo "================================================================================================================="
+
+echo
+echo "Checking the presence of required tools"
+# see http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+for i in identify convert ant
+do
+    echo "  Checking command $i"
+    command -v $i >/dev/null 2>&1 || { echo >&2 "$i not found"; exit 1; }
+done
+IDENTIFY=identify
+CONVERT=convert
+ANT=ant
+
+echo "  All tools found."
+echo
+
+
+PWD=$(pwd)
+TDIR="../"$(basename "${PWD}")"-imgindex/"
+mkdir -p "${TDIR}"
+
+
+
+# thumbnail sizes
+SIZES="250 400 800 1280 1920 2500 3840"
+
+# Output file
+#XML="${TDIR}/index.xml"
+XML="./index.xml"
+
+
+
+echo
+echo "Settings:"
+echo "  Target directory: \"${TDIR}\""
+echo "  Thumbnail sizes:  $SIZES"
+echo "  Index XML:        \"${XML}\""
+
+# Write XML header =====================================================================================================
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" > "${XML}"
+echo "<panopage>" >> "${XML}"
+echo "    <!-- Thumbnail sizes (bounding box)  -->" >> "${XML}"
+echo "    <sizes>" >> "${XML}"
+for S in $SIZES
+do
+    echo "        <size>"${S}"</size>" >> "${XML}"
+done
+echo "    </sizes>" >> "${XML}"
+
+
+echo "    <images>" >> "${XML}"
+
+
+
+{ find -type f -name "*.jpg" ; \
+  find -type f -name "*.JPG" ; \
+  find -type f -name "*.jpeg" ; \
+  find -type f -name "*.JPEG" ; \
+  find -type f -name "*.png" ; \
+  find -type f -name "*.PNG" ; \
+  find -type f -name "*.tif" ; \
+  find -type f -name "*.TIF" ;  \
+  find -type f -name "*.tiff" ; \
+  find -type f -name "*.TIFF" ; } | while read i
+do
+    echo
+    echo "-----------------------------------------------------------------------------------------------------"
+    echo "Process image \"$i\""
+    echo "-----------------------------------------------------------------------------------------------------"
+    echo
+
+
+    #    if [[ "$i" == */zoomify/* || "$i" == */ZOOMIFY/* ]]
+    #    then
+    #        echo "    Image $i is zoomify file, skip it"
+    #        continue
+    #    fi
+
+    if [ ! -e "${i}" ] ; then echo "Image not found, continue" ; continue ; fi
+
+    F=`basename "${i}"`
+    F=`basename "${F}" .jpg`
+    F=`basename "${F}" .JPG`
+    F=`basename "${F}" .jpeg`
+    F=`basename "${F}" .JPEG`
+    F=`basename "${F}" .png`
+    F=`basename "${F}" .PNG`
+    F=`basename "${F}" .tif`
+    F=`basename "${F}" .TIFF`
+
+    ORIG=`echo "${i}" | sed -e "s/.\\///"`
+
+    echo "    F (Image base)          \"${F}\""
+    echo "    ORIG (Orig link)        \"${ORIG}\""
+
+    # Basic file info --------------------------------------------------------------------------------------------------
+    echo "        <image>" >> "${XML}"
+    echo -n "            <original>${ORIG}</original>" >> "${XML}" # Identify seemingly clips leading spaces from format string
+    ${IDENTIFY} -format "\n            <width>%w</width>\n            <height>%h</height>" "${i}" >> "${XML}"
+    echo "            <size>"`stat --printf="%s" "${i}"`"</size>" >> "${XML}"
+
+    # check for not so important pics folder ---------------------------------------------------------------------------
+    if [[ "$i" == */_nti/* || "$i" == */_NTI_/* || "$i" == */_nsi/* || "$i" == */_NSI_/* ]]
+    then
+        echo "    _nti (not too important)"
+        echo "            <nti>true</nti>" >> "${XML}"
+    else
+        echo "            <nti>false</nti>" >> "${XML}"
+    fi
+
+    # check for panorama src/workfile ----------------------------------------------------------------------------------
+    if [[ "$i" == */sources/* || "$i" == */SOURCES/* ]]
+    then
+        echo "    source (panorama) file"
+        echo "            <panosource>true</panosource>" >> "${XML}"
+    else
+        echo "            <panosource>false</panosource>" >> "${XML}"
+    fi
+    if [[ "$i" == */workfiles/* || "$i" == */WORKFILES/* ]]
+    then
+        echo "    panorama workfile"
+        echo "            <panoworkfile>true</panoworkfile>" >> "${XML}"
+    else
+        echo "            <panoworkfile>false</panoworkfile>" >> "${XML}"
+    fi
+
+    # cache / store EXIF data ==========================================================================================
+    EXIFDIR="${TDIR}/exif"
+    SUBDIR="${EXIFDIR}/"`dirname "${i}"`/
+    SUBDIR=$(echo "${SUBDIR}" | sed -e "s/\\/.\\//\\//" | sed -e "s/\\/\\//\\//")
+    TF="${SUBDIR}${F}.exif"
+    TFX="${SUBDIR}${F}.exif.xmlpart"
+
+    echo "    EXIF cache subdir:      \"${SUBDIR}\""
+    echo "    EXIF cache file:        \"${TF}\""
+    echo "    EXIF XML fragment file: \"${TFX}\""
+    mkdir -p "${SUBDIR}"
+
+    if [ ! -e "$TF" ]
+    then
+        echo "    Exif cache file not found; launch identity to check / extract EXIF header"
+        EXIF=`${IDENTIFY} -format "%[exif:*]" "${i}"`
+        echo $EXIF > "$TF"
+        if [ "${EXIF}" != "" ]
+        then
+            echo "        Exif in image file found; retrieve Exif XML fragment"
+            echo "            <exif>" > "$TFX"
+            ${IDENTIFY} -format "%[exif:*]" "${i}" | grep -v "^$" | sed -e "s/^exif://" | tr "=" " " | awk '{ printf "                <%s>",$1 ; for(i=2;i<=NF;i++) { printf i==2?"%s":" %s",$i ; } printf "</%s>\n",$1 ; }' >> "$TFX"
+            echo "            </exif>" >> "$TFX"
+        fi
+    fi
+
+    if [ -e "${TFX}" ]
+    then
+        cat "$TFX" >> "${XML}"
+    fi
+
+    TMBDIR="${TDIR}/thumbnail"
+    # Create / reference thumbnails ====================================================================================
+    for S in $SIZES
+    do
+        #TMBDIR="${TDIR}/tmb-${S}"
+        SUBDIR="${TMBDIR}/"`dirname "${i}"`/
+        SUBDIR=`echo ${SUBDIR} | sed -e "s/\\/.\\//\\//" | sed -e "s/\\/\\//\\//"`
+        TF="${SUBDIR}${F}-tmb-${S}.jpg"
+        mkdir -p "${SUBDIR}"
+
+        echo "        Thumbnail ${S} x ${S}:"
+        echo "            Subdir:         ${SUBDIR}"
+        echo "            Thumbnail file: ${TF}"
+
+        echo "            <thumbnail size=\""${S}"\">" >> "${XML}"
+        echo -n "                <file>"${TF}"</file>" >> "${XML}" # Subsequent identify space clipping
+
+        if [ ! -e "${TF}" ]
+	then
+	    echo "            Thumbnail missing, create"
+
+            # auto orient thumbnails
+            # see http://stackoverflow.com/questions/19456036/detect-exif-orientation-and-rotate-image-using-imagemagick
+            nice -19 ${CONVERT} "${i}" -auto-orient -quality 85 -resize ${S}"x"${S} "${TF}"
+        fi
+
+        ${IDENTIFY} -format '\n                <width>%w</width>\n                <height>%h</height>' "${TF}" >> "${XML}"
+        echo "                <size>"`stat --printf="%s" "${TF}"`"</size>" >> "${XML}"
+
+	echo "            </thumbnail>" >> "${XML}"
+
+    done
+
+    # Close image tag
+    echo "        </image>" >> "${XML}"
+
+done
+
+
+echo "    </images>" >> "${XML}"
+echo "</panopage>" >> "${XML}"
+
+
+echo "Translate "${XML}
+# process summary
+cp `dirname $0`/pic-page-build.xml .
+cp `dirname $0`/pic-page-style.xslt .
+ant -f pic-page-build.xml -Douthtml=index.html picpage
---
+++ pic-page-style.xslt
@@ -0,0 +1,196 @@
+<?xml version="1.0"?>
+<!DOCTYPE xsl:stylesheet [ <!ENTITY nbsp "&#160;"> ]>
+
+
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:stringutils="xalan://org.apache.tools.ant.util.StringUtils" xmlns:redirect="http://xml.apache.org/xalan/redirect" extension-element-prefixes="redirect">
+    <!-- <xsl:output method="html" indent="yes" encoding="US-ASCII" doctype-public="-//W3C//DTD HTML 4.01 Transitional//EN" /> -->
+    <xsl:output method="html" indent="yes" encoding="US-ASCII" />
+
+
+<xsl:template match="panopage">
+
+    <html><head><title>Redirect to thumbnails page</title><meta http-equiv="Refresh" content="0; URL=html/preview-800.html"/></head></html>
+
+    <xsl:for-each select="./sizes/size">
+	<xsl:call-template name="mkprevpage">
+	    <xsl:with-param name="size" select="."/>
+	    <xsl:with-param name="filename">html/preview-<xsl:value-of select="."/>.html</xsl:with-param>
+	</xsl:call-template>
+    </xsl:for-each>
+
+
+</xsl:template>
+
+
+
+<xsl:template name="mkprevpage">
+    <xsl:param name="size" select="1280"/>
+    <xsl:param name="filename" select="html/default"/>
+    
+    <redirect:write file="{$filename}">
+	<html><head><title><xsl:value-of select="$size"/></title>
+	    <xsl:call-template name="css"/>
+	</head><body>
+	
+	    <div style="text-align: right; border-bottom: 1px solid grey; padding: 4px;">Preview image size: 
+		<xsl:for-each select="/panopage/sizes/size">
+		    <xsl:if test="position() > 1">
+			|
+		    </xsl:if>
+		    <a><xsl:attribute name="href">preview-<xsl:value-of select="."/>.html</xsl:attribute><xsl:value-of select="."/>px</a>
+		</xsl:for-each>
+	    </div>
+	
+	    <center><table border="0" cellspacing="0" cellpadding="2px">
+	
+		<xsl:for-each select="/panopage/images/image[ panosource != 'true' and panoworkfile != 'true' and nti != 'true' ]">
+		    <xsl:sort select="exif/DateTime"/>
+		    <xsl:sort select="original"/>
+		    <tr>
+			<td>
+			    <xsl:if test="position() > 1"><xsl:attribute name="style">border-top: 1px solid black;</xsl:attribute></xsl:if>
+			    <xsl:value-of select="position()"/>
+			</td>
+		        <td rowspan="1">
+			    <xsl:if test="position() > 1"><xsl:attribute name="style">border-top: 1px solid black;</xsl:attribute></xsl:if>
+		            <center><img>
+				<xsl:attribute name="src"> ../<xsl:value-of select="./thumbnail[ @size = $size ]/file"/></xsl:attribute>
+			    </img></center>
+		        </td>
+			<td>
+			    <xsl:if test="position() > 1"><xsl:attribute name="style">border-top: 1px solid black;</xsl:attribute></xsl:if>
+			    <table border="0">
+				<tr>
+				    <td>    
+					Original&nbsp;file: 
+				    </td>
+				    <td colspan="3">    
+					<b><a><xsl:attribute name="href">../<xsl:value-of select="./original"/></xsl:attribute><xsl:value-of select="./original"/></a></b>
+				    </td>
+				    
+				</tr>
+				
+				<xsl:if test="exif">
+				    <tr><td>DateTime</td><td colspan="3"><b><xsl:value-of select="exif/DateTime"/></b></td></tr>
+				    <tr><td>Make, model</td><td colspan="3"><b><xsl:value-of select="exif/Make"/>, <xsl:value-of select="exif/Model"/></b></td></tr>
+					
+				</xsl:if>
+				
+				<tr>
+				    <td>
+					File&nbsp;size:
+				    </td>
+				    <td style="text-align: right;">
+					<b><xsl:value-of select="format-number(./size div 1000000,'0.0')"/></b>
+				    </td>
+				    <td>
+					MiB
+				    </td>
+				    <td width="100%"> </td>
+				</tr>
+				<tr>
+				    <td>
+					Image&nbsp;width:
+				    </td>
+				    <td style="text-align: right;">
+					<b><xsl:value-of select="./width"/></b>
+				    </td>
+				    <td>
+				        px
+				    </td>
+				</tr>
+				<tr>
+				    <td>
+					Image&nbsp;height:
+				    </td>
+				    <td style="text-align: right;">
+					<b><xsl:value-of select="./height"/></b>
+				    </td>
+				    <td>
+				        px
+				    </td>
+				</tr>
+				<tr>
+				    <td>
+					Image&nbsp;size:
+				    </td>
+				    <td style="text-align: right;">
+					<b><xsl:value-of select="format-number(./width * ./height div 1000000,'0.0')"/></b>
+				    </td>
+				    <td>
+					MPixel
+		    		    </td>
+		    		</tr>
+				<tr>
+				    <td>
+					Your opinion:
+				    </td>
+				    <td colspan="3">
+					<a><xsl:attribute name="href">mailto:gimre@chemaxon.com?subject=MUSTHAVE: <xsl:value-of select="original"/></xsl:attribute>Must have</a> | 
+					<a><xsl:attribute name="href">mailto:gimre@chemaxon.com?subject=MAYBE: <xsl:value-of select="original"/></xsl:attribute>Maybe</a> | 
+					<a><xsl:attribute name="href">mailto:gimre@chemaxon.com?subject=SUCKS: <xsl:value-of select="original"/></xsl:attribute>This sucks</a>  
+				    </td>
+				</tr>
+				<tr>
+				    <td>
+				        Preview
+				    </td>
+				    <td colspan="3">
+					<xsl:for-each select="./thumbnail">
+					    <xsl:if test="position() > 1">
+						|
+					    </xsl:if>
+					    <a target="_blank">
+						<xsl:attribute name="href">../<xsl:value-of select="file"/></xsl:attribute>
+						<xsl:value-of select="@size"/>
+					    </a>
+					</xsl:for-each>
+				    </td>
+				</tr>
+			    </table>
+			</td>
+		    </tr>
+			
+		</xsl:for-each>
+	
+	    </table></center>
+	
+	
+	</body></html>	
+        
+    </redirect:write>
+
+</xsl:template>
+
+
+    
+
+<!-- template for embedded css ########################################################################### -->
+<xsl:template name="css">
+        <style type="text/css">
+            body { font:normal 68% verdana,arial,helvetica; color:#000000; margin: 0px;}
+            table tr td, table tr th { font-size: 68%; }
+            table.details tr th { font-weight: bold; text-align:left; background:#a6caf0; }
+            table.details tr td { background:#eeeee0; }
+            p { line-height:1.5em; margin-top:0.5em; margin-bottom:1.0em; }
+            h1 { margin: 0px 0px 5px; font: 165% verdana,arial,helvetica }
+            h2 { margin-top: 1em; margin-bottom: 0.5em; font: bold 125% verdana,arial,helvetica }
+            h3 { margin-bottom: 0.5em; font: bold 115% verdana,arial,helvetica }
+            h4 { margin-bottom: 0.5em; font: bold 100% verdana,arial,helvetica }
+            h5 { margin-bottom: 0.5em; font: bold 100% verdana,arial,helvetica }
+            h6 { margin-bottom: 0.5em; font: bold 100% verdana,arial,helvetica }
+            .Error { font-weight:bold; color:red; }
+            .Failure { font-weight:bold; color:purple; }
+            .Properties { text-align:right; }
+            .Outlinks { text-align:left; }
+            .Out { font-weight:normal; color: black; margin-top:0em; margin-bottom:0em; }
+        </style>
+</xsl:template>
+
+<!-- File names  ######################################################################################### -->
+
+
+<!-- Write files ######################################################################################### -->
+
+
+</xsl:stylesheet>
