c473a4dfee10986d8d6378c9a1cf243dbf6deee3
Gabor Imre <imregabor@gmail.com>
2023-10-01 19:06:10 +0200

    Formatting, option -nb to skip stitch/blend

--- pano-smaller.sh
+++ pano-smaller.sh
@@ -11,16 +11,32 @@ set -u
 # see http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 for i in nona enblend pto_gen pano_modify cpfind cpclean autooptimiser pto_var vig_optimize convert 
 do
-    echo "Checking command $i"
-    command -v $i >/dev/null 2>&1 || { echo >&2 "$i not found"; exit 1; }
-done    
+  echo "Checking command $i"
+  command -v $i >/dev/null 2>&1 || { echo >&2 "$i not found"; exit 1; }
+done
 
 CONVERTCMD=convert
 if command -v magick >/dev/null 2>&1; then
-	CONVERTCMD=magick
+    CONVERTCMD=magick
     echo "Use $CONVERTCMD for imagemagick convert"
 fi
 
+DOBLEND=true
+
+# from https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -nb)
+      DOBLEND=false
+      echo "Will not stitch / blend"
+      shift
+      ;;
+    *)
+      echo "Unknown option \"$1\""
+      exit 1
+      ;;
+  esac
+done
 
 
 LOG=pano.log
@@ -35,75 +51,69 @@ PMCRP="--crop=AUTO"
 PMCTR="--center"
 STARTSEC=$(date "+%s")
 
-
 function sect {
-    TS=$(date "+%Y-%m-%d %H:%M:%S")
-	DT=$(( $(date "+%s") - $STARTSEC ))
-	DTF=$(printf "%5d" $DT)
-    echo | tee -a "${LOG}" ; 
-	echo | tee -a "${LOG}" ; 
-	echo "+--[ ${TS}, ${DTF} s ]--------------------------------------------------------------------------------------" | tee -a "${LOG}" 
-	echo "|" | tee -a "${LOG}"
-    while [ $# -gt 0 ] ; do echo "| $1" | tee -a "${LOG}"; shift; done
-    echo "|" | tee -a "${LOG}"
-	echo "| DT: ${DT} s" | tee -a "${LOG}"
-    echo "|" | tee -a "${LOG}" ; 
-	echo "+------------------------------------------------------------------------------------------------------------------------" | tee -a "${LOG}" ; 
-	echo | tee -a "${LOG}" ; 
-	echo | tee -a "${LOG}"
+  TS=$(date "+%Y-%m-%d %H:%M:%S")
+  DT=$(( $(date "+%s") - $STARTSEC ))
+  DTF=$(printf "%5d" $DT)
+  echo | tee -a "${LOG}"
+  echo | tee -a "${LOG}"
+  echo "+--[ ${TS}, ${DTF} s ]--------------------------------------------------------------------------------------" | tee -a "${LOG}"
+  echo "|" | tee -a "${LOG}"
+  while [ $# -gt 0 ] ; do echo "| $1" | tee -a "${LOG}"; shift; done
+  echo "|" | tee -a "${LOG}"
+  echo "| DT: ${DT} s" | tee -a "${LOG}"
+  echo "|" | tee -a "${LOG}" ;
+  echo "+------------------------------------------------------------------------------------------------------------------------" | tee -a "${LOG}"
+  echo | tee -a "${LOG}" ;
+  echo | tee -a "${LOG}"
 }
 
 function blprev {
-    sect "Launch stitch/blend preview" "Input file: $1" "Output basename: $2" 
+  sect "Launch stitch/blend preview" "Input file: $1" "Output basename: $2"
 
-	if [ ! -e "$1" ]; then
-	    echo "ERROR! No input pto file specified, exiting"
-		exit -1
-	fi
-	if [ -z "$2" ]; then 
-	    echo "ERROR! No output basename specified, exiting"
-		exit -1
-	fi
-    if [ ! -d "workfiles" ] ; then
-		echo "Create workfiles/ dir"
-        mkdir workfiles/
-    fi
-	
-	
+  if [ ! -e "$1" ]; then
+    echo "ERROR! No input pto file specified, exiting"
+    exit -1
+  fi
 
-    # Nona tif prefix
-    #NONAO=workfiles/`basename "${PO}" ".pto"`-
-	NONAO="workfiles/${2}-"
+  if [ -z "$2" ]; then
+    echo "ERROR! No output basename specified, exiting"
+    exit -1
+  fi
 
-    # Blended output file
-    #ENBLO=workfiles/`basename "${PO}" ".pto"`-blend.tif
-	ENBLO="workfiles/${2}-blend.tif"
-	
-	JPG="workfiles/${2}-blend.jpg"
+  if [ ! -d "workfiles" ] ; then
+    echo "Create workfiles/ dir"
+    mkdir workfiles/
+  fi
 
-    sect "Stitch with nona" "NONAO: \"${NONAO}\""
-	nona -o "${NONAO}" -m TIFF_m "${PO}" 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
-    
-	sect "Blend with enblend" "ENBLO: \"${ENBLO}\"" 
-	enblend -o "${ENBLO}" "${NONAO}"*.tif 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
-	
-	sect "Create jpg from last stitch" "ENBLO: \"${ENBLO}\"" "JPG:   \"${JPG}\""
-	$CONVERTCMD "${ENBLO}" -quality 100 "${JPG}" 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
+  # Nona tif prefix
+  #NONAO=workfiles/`basename "${PO}" ".pto"`-
+  NONAO="workfiles/${2}-"
 
+  # Blended output file
+  #ENBLO=workfiles/`basename "${PO}" ".pto"`-blend.tif
+  ENBLO="workfiles/${2}-blend.tif"
+
+  JPG="workfiles/${2}-blend.jpg"
+
+  sect "Stitch with nona" "NONAO: \"${NONAO}\""
+  nona -o "${NONAO}" -m TIFF_m "${PO}" -v --ignore-exposure 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
+
+  sect "Blend with enblend" "ENBLO: \"${ENBLO}\""
+  enblend -v -o "${ENBLO}" "${NONAO}"*.tif 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
+
+  sect "Create jpg from last stitch" "ENBLO: \"${ENBLO}\"" "JPG:   \"${JPG}\""
+  $CONVERTCMD "${ENBLO}" -quality 100 "${JPG}" 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
 }
 
-
 sect "Prepare panorama initial workflow" "    pwd: $(pwd)"
-
 
 if [ ! -d "sources/" ]
 then
     echo "Create sources/ dir; move sources"
     mkdir sources/
-    
     mv *.[jJ][pP][gG] sources/
 fi
-
 
 # input PTO file
 PI=""
@@ -115,21 +125,20 @@ POCT=0;
 #  $1 - short name, will be part of new output file name
 #  $2 - description, will be printed 
 function newPto() {
-    if [ -z "$1" ]; then 
-        echo "ERROR: No short name specified, exiting."
-		exit -1
-	fi
-	
-	if [ -z "$2" ]; then
-		echo "ERROR: No human readable description specified, exiting."
-		exit -1
-	fi
-		
-    PI="${PO}"
-	PO="sources/pano-$(printf '%02d' $POCT)-$1.pto"
-	POCT=$(( $POCT + 1))
-	sect "$2" "  PI: ${PI}" "  PO: ${PO}"
-	
+  if [ -z "$1" ]; then
+    echo "ERROR: No short name specified, exiting."
+    exit -1
+  fi
+
+  if [ -z "$2" ]; then
+    echo "ERROR: No human readable description specified, exiting."
+    exit -1
+  fi
+
+  PI="${PO}"
+  PO="sources/pano-$(printf '%02d' $POCT)-$1.pto"
+  POCT=$(( $POCT + 1))
+  sect "$2" "  PI: ${PI}" "  PO: ${PO}"
 }
 
 newPto "initial" "pto-gen from source JPGs"
@@ -162,19 +171,26 @@ autooptimiser -n -o "${PO}" "${PI}" 2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
 newPto "geomopted-m" "modify: straighten, center, fov"
 pano_modify -o "${PO}" --straighten $PMCTR $PMFOV $PMCRP "${PI}"  2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
 
-newPto "vignopt" "set photometric optimization (vignetting)"
-pto_var -o "${PO}" --modify-opt --opt Ra,Rb,Rc,Rd,Re,Vb,Vc,Vd,Vx,Vy "${PI}"  2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
 
-newPto "vignopted" "do photometric optimization"
-vig_optimize -o "${PO}" -v -p 5000 "${PI}"    2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
+# newPto "vignopt" "set photometric optimization (vignetting)"
+# pto_var -o "${PO}" --modify-opt --opt Ra,Rb,Rc,Rd,Re,Vb,Vc,Vd,Vx,Vy "${PI}"  2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
+#
+# newPto "vignopted" "do photometric optimization"
+# vig_optimize -o "${PO}" -v -p 5000 "${PI}"    2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
 
 newPto "final" "set final canvas size and crop"
 pano_modify -o "${PO}" --straighten $PMCTR --canvas=AUTO $PMFOV $PMCRP "${PI}"  2>&1 | sed -ue "s/^/    /" | tee -a "${LOG}"
-            
-
-# Final blend
-blprev "${PO}" p1
 
 
+if [ "$DOBLEND" = true ] ; then
+  # Final blend
+  blprev "${PO}" p1
+else
+  echo
+  echo
+  echo "Skipping stitch/blend"
+  echo
+  echo
+fi
 
 sect "All done."
