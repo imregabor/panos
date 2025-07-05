c85c4d60974de840a1cb1e7e80b8e50692c1a8f3
Gabor Imre <imregabor@gmail.com>
2025-07-06 00:31:25 +0200

    Invoke pano-smaller.sh in immediate subdirs

---
+++ subdirs-pano-smaller.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Find all immediate child directories from the current location
+# and invoke pano-smaller.sh in all of them.
+#
+find -maxdepth 1 -mindepth 1 -type d | while read -r dir; do
+    (cd "$dir" && "$(dirname "$0")/pano-smaller.sh")
+done
