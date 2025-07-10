d63f976765506456c8020bc2ea375e490318a7ba
Gabor Imre <imregabor@gmail.com>
2025-07-10 14:24:56 +0200

    Traverse directories ordered by size

--- subdirs-pano-smaller.sh
+++ subdirs-pano-smaller.sh
@@ -3,6 +3,21 @@
 # Find all immediate child directories from the current location
 # and invoke pano-smaller.sh in all of them.
 #
-find -maxdepth 1 -mindepth 1 -type d | while read -r dir; do
-    (cd "$dir" && "$(dirname "$0")/pano-smaller.sh" "$@")
+# Directories will be travesed from smaller to larger
+# see https://unix.stackexchange.com/questions/106330/sort-all-directories-based-on-their-size
+#
+
+echo
+echo
+echo "Directories to traverse:"
+echo
+
+du -sh -- */ | sort -h
+
+echo
+echo
+echo
+
+du -s -- */ | sort -n | cut -f2 | while read -r dir; do
+  (cd "$dir" && "$(dirname "$0")/pano-smaller.sh" "$@")
 done
